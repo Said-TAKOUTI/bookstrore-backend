9f812a7cf93f74fc73bff8582d8b2edc9c7e0949
mohamed <takouti.said@outlook.com>
2024-12-17 03:39:16 +0100

    Add payment using Stripe

--- src/main/java/com/takouti/bookstore/service/CheckoutService.java
+++ src/main/java/com/takouti/bookstore/service/CheckoutService.java
@@ -1,9 +1,14 @@
 package com.takouti.bookstore.service;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.takouti.bookstore.dto.PaymentInfo;
 import com.takouti.bookstore.dto.Purchase;
 import com.takouti.bookstore.dto.PurchaseResponse;
 
 public interface CheckoutService {
 
     PurchaseResponse placeOrder(Purchase purchase);
+
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
--- src/main/java/com/takouti/bookstore/service/CheckoutServiceImpl.java
+++ src/main/java/com/takouti/bookstore/service/CheckoutServiceImpl.java
@@ -1,24 +1,30 @@
 package com.takouti.bookstore.service;
 
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import com.takouti.bookstore.dao.CustomerRepository;
+import com.takouti.bookstore.dto.PaymentInfo;
 import com.takouti.bookstore.dto.Purchase;
 import com.takouti.bookstore.dto.PurchaseResponse;
 import com.takouti.bookstore.entity.Customer;
 import com.takouti.bookstore.entity.OrderItem;
 import com.takouti.bookstore.entity.Order;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 
     private final CustomerRepository customerRepository;
 
-    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+    public CheckoutServiceImpl(CustomerRepository customerRepository, @Value("${stripe.key.secret}") String secretKey) {
         this.customerRepository = customerRepository;
+
+        Stripe.apiKey = secretKey;
     }
     @Override
     @Transactional
@@ -62,6 +68,20 @@ public class CheckoutServiceImpl implements CheckoutService {
         // return a response
         return new PurchaseResponse(orderTrackingNumber);
 
+    }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currency", paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "bookstore PURCHASE");
+        params.put("receipt_email", paymentInfo.getReceiptEmail());
+        return PaymentIntent.create(params);
     }
 
     private String generateOrderTrackingNumber() {
